cdfe548862f793aabe607709a15e379ceb279c65
0sm0s1z <mtoussain@gmail.com>
2016-02-29 10:59:41 -0600

    Basic Ravenclaw Integration

---
+++ cmd.php
@@ -0,0 +1,1 @@
+VOYhfP4nfLSwJW693RdQuw==
--- database.sql
+++ database.sql
@@ -27,4 +27,35 @@ CREATE TABLE C2_Profiles(
    Status         VARCHAR(100)
 );
 
+CREATE TABLE Channels(
+   ID INTEGER PRIMARY KEY   AUTOINCREMENT,
+   HostID         VARCHAR(100)   NOT NULL,
+   Details     	  VARCHAR(100),
+   Tags           VARCHAR(100)
+);
 
+insert into Interactions (hostid, channelid, command, response, status)
+VALUES ("1", "1", "calc", "", "0");
+insert into Interactions (hostid, channelid, command, response, status)
+VALUES ("1", "1", "notepad", "", "0");
+insert into Interactions (hostid, channelid, command, response, status)
+VALUES ("1", "1", "ls", "", "0");
+
+
+CREATE TABLE Interactions(
+   ID INTEGER PRIMARY KEY   AUTOINCREMENT,
+   HostID           VARCHAR(100),
+   ChannelID        VARCHAR(100)   NOT NULL,
+   Command          VARCHAR(1000),
+   Response         VARCHAR(100),
+   Status           VARCHAR(100)
+);
+
+SELECT * FROM Hosts;
+SELECT * FROM Channels;
+SELECT * FROM Interactions;
+
+
+DROP TABLE Hosts;
+DROP TABLE Channels;
+DROP TABLE Interactions;
